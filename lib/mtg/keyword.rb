014c02e5677edebac21161fd51471f963e60dd2d
Ben Prew <ben.prew@gmail.com>
2009-01-08 18:12:23 -0700

    more matching improvements, translate numbers correctly 8th -> eigth, 2nd -> second, etc.  only store top 3 matches instead of top 5, since that table is growing rapidly

--- lib/mtg/keyword.rb
+++ lib/mtg/keyword.rb
@@ -2,7 +2,21 @@ module Keyword
 
   # only returns keywords that are 3 chars or more
 
-  @@reject_list = %w(dci the mtg promo gathering jss fnm mint rare prerelease prix tcg textless)
+  @@reject_list = %w(dci the mtg promo gathering jss fnm mint rare prerelease prix tcg textless excellent)
+
+  @@numbers = {
+    '1st' => 'first',
+    '2nd' => 'second',
+    '3rd' => 'third',
+    '4th' => 'fourth',
+    '5th' => 'fifth',
+    '6th' => 'sixth',
+    '7th' => 'seventh',
+    '8th' => 'eighth',
+    '9th' => 'ninth',
+    '10th' => 'tenth',
+    '11th' => 'eleventh'
+  }
   
   def keywords_from_string(string)
     str = string.gsub(/'/, '')
@@ -10,7 +24,7 @@ module Keyword
     str.downcase!
     str = str.gsub(/free shipping/, ' ')
     str = str.gsub(/magic the gathering/, ' ')
-    str.split(/\s+/).reject { |i| @@reject_list.member?(i) }.grep(/\w{3,}/)
+    str.split(/\s+/).reject { |i| @@reject_list.member?(i) }.grep(/\w{3,}/).map { |j| @@numbers.has_key?(j) ? @@numbers[j] : j }
   end
 end
 
