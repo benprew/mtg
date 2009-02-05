495345ee47e284fccda67f6eb601d33bc8ce334b
Ben Prew <ben.prew@gmail.com>
2009-02-04 21:38:16 -0700

    add new script to show keywords from either a card or a string

---
+++ keywords.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'mtg/keyword'
+require 'mtg/db'
+require 'mtg/card'
+
+include Keyword
+
+if ARGV[0].match(/^\d+$/)
+  p Card.get(ARGV[0]).all_keywords
+else
+  p keywords_from_string(ARGV[0])
+end
