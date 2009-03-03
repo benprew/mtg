e58ffca5c1249f64a8a30bded3aaabb425c6ef70
Ben Prew <ben.prew@gmail.com>
2009-03-02 17:44:15 -0700

    stop freezing and unfreezing card keywords, since it takes almost no time and just makes caching bugs more apparent

--- lib/mtg/matcher.rb
+++ lib/mtg/matcher.rb
@@ -6,19 +6,11 @@ class Matcher
 
   attr_accessor :cards_keywords
 
-  @@cards_keywords_filename = 'cards_keywords'
-
   def initialize
     if !@cards_keywords
-      warn "thawing keywords"
-      _thaw_cards_keywords()
-      if !@cards_keywords
-        warn "building keywords"
-        @cards_keywords = {}
-        _build_keywords_from_cards()
-        warn "freezing keywords"
-        _freeze_cards_keywords()
-      end
+      warn "building keywords"
+      @cards_keywords = {}
+      _build_keywords_from_cards()
     end
 
     @average_num_keywords =
@@ -34,23 +26,9 @@ class Matcher
     end
   end
 
-  def _thaw_cards_keywords
-    if File.exists?(@@cards_keywords_filename)
-      f = File.new(@@cards_keywords_filename, 'r')
-      @cards_keywords = Marshal.restore(f.gets(nil))
-    end
-  end
-
-  def _freeze_cards_keywords
-    f = File.new(@@cards_keywords_filename, 'w')
-    f.puts(Marshal.dump(@cards_keywords))
-    f.close
-  end
-
   def _points_for_keyword(keyword)
     @cards_keywords[keyword].length >= @average_num_keywords ? 1 : 1.5
   end
-
 
   def match(description)
     description_keywords = keywords_from_string(description)
