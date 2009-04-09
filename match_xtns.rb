39567365973efa35e28fbce923a14bccc5b0e788
Ben Prew <ben.prew@gmail.com>
2009-04-09 00:23:38 -0600

    convert trie-style matching to be the primary matching system

--- match_xtns.rb
+++ match_xtns.rb
@@ -2,80 +2,74 @@
 
 $:.unshift File.dirname(__FILE__) + '/lib'
 
-require 'rubygems'
-require 'dm-core'
-require 'mtg/external_item'
-require 'mtg/matcher'
-require 'mtg/card'
-require 'mtg/possible_match'
+require 'trie'
 require 'mtg/db'
+require 'logger'
+require 'mtg/external_item'
+require 'mtg/trie_matcher'
 
-def _match_card(item, card_no)
-  log.debug "matching card: #{card_no}"
-  e = ExternalItem.first(item['external_item_id'])
-  e.card_no = card_no
-  e.cards_in_item = _cards_in_item(item['description'])
-  e.save
-end
+class XtnMatcher < Logger::Application
 
-def _cards_in_description(description)
-  m = description.match(/(x\s*(\d+)|(\d)+\s*x)/i)
-  if m
-    return m[2] || m[3]
-  else
-    return 1
-  end
-end
-
-def _save_possible_matches(ext_item, possible_matches)
-  repository(:default).adapter.execute('DELETE FROM possible_matches WHERE external_item_id = ?', [ext_item.external_item_id])
-  possible_matches.each do |pm|
-    PossibleMatch.create(
-      :external_item_id => ext_item.external_item_id,
-      :card_no => pm[0],
-      :score => pm[1]
-    ).save
-  end
-end
-
-##############
-
-m = Matcher.new()
-
-warn "done building card keywords"
-count = 0
-
-# q('SELECT description, external_item_id FROM external_items e LEFT OUTER JOIN possible_matches pm USING (external_item_id) WHERE e.card_no IS null and pm.card_no IS null GROUP BY external_item_id')
-ExternalItem.all(:card_no => nil).each do |i|
-  count += 1
-  warn "#{count} items #{Time.now()}" if count % 100 == 0
-
-  next if PossibleMatch.first(:external_item_id => i.external_item_id)
-
-  # There are a lot of "extended art" cards on ebay now, and they sell for a
-  # lot more then the actual card, so we want to match them to "not a card"
-  if i.description.match(/(extended|altered).*art/i)
-    _match_card(i, -1)
-    next
+  def initialize
+    super('XtnMatcher')
   end
 
-  possible_matches = m.match(i.description)
-  next unless possible_matches.length > 0
-
-  _save_possible_matches(i, possible_matches)
-
-  if possible_matches[0][1] >= 10
-    warn " #{i.description} ##### #{Card.get(possible_matches[0][0]).name}"
-    p possible_matches
+  def q(sql, bind_params = [])
+    return repository(:default).adapter.query(sql, bind_params)
   end
 
-  if possible_matches.length == 1
-    if possible_matches[0][1] >= 10 
-      _match_card(i, possible_matches[0][0])
+  def run
+    @log.level = Logger::DEBUG
+
+    @log.info("Building matcher")
+    @matcher = TrieMatcher.new(@log)
+
+    @log.info("Loading external items")
+    ext_items = q('SELECT description, external_item_id FROM external_items e LEFT OUTER JOIN possible_matches pm USING (external_item_id) WHERE e.card_no IS null and pm.card_no IS null GROUP BY external_item_id')
+    
+    @log.info("Matching #{ext_items.length} items")
+    ext_items.each do |item|
+      possible_matches = @matcher.match(item)
+      if !possible_matches || possible_matches.length == 0
+        @log.debug "no matches for #{item['description']}"
+      elsif possible_matches.length == 1
+        @log.debug "matching card: #{possible_matches[0]}"
+        _match_card(item, possible_matches[0])
+      else
+        @log.debug "saving #{possible_matches.length} possible matches"
+        _save_possible_matches(item['external_item_id'], possible_matches)
+      end
     end
-  elsif possible_matches[0][1] - 9 >= possible_matches[1][1]
-      _match_card(i, possible_matches[0][0])
+  end
+
+  def _match_card(item, card_no)
+    e = ExternalItem.get(item['external_item_id'])
+    e.card_no = card_no
+    e.cards_in_item = _cards_in_description(item['description'])
+    e.save
+  end
+
+  def _cards_in_description(description)
+    m = description.match(/(x\s*(\d+)|(\d)+\s*x)/i)
+    if m
+      return m[2] || m[3]
+    else
+      return 1
+    end
+  end
+
+  def _save_possible_matches(ext_item_id, possible_matches)
+    repository(:default).adapter.execute('DELETE FROM possible_matches WHERE external_item_id = ?', [ext_item_id])
+    possible_matches.each do |pm|
+      PossibleMatch.create(
+        :external_item_id => ext_item.external_item_id,
+        :card_no => pm,
+        :score => 1
+      ).save
+    end
   end
 end
+
+XtnMatcher.new().start()
 
 
