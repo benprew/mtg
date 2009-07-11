a670f4b666f136254f78a09a21de7e9186a657fc
Ben Prew <ben.prew@gmail.com>
2009-07-11 02:05:03 -0700

    initial rev. to import card images from sets

---
+++ create_images.rb
@@ -0,0 +1,10 @@
+#!/usr/local/ruby/bin/ruby
+
+require 'rubygems'
+require 'hpricot'
+
+doc = open(ARGV[0]) { |f| Hpricot(f) }
+
+doc.search("div.visualspoiler").search("img") do |img|
+  puts img.get_attribute :alt
+end
