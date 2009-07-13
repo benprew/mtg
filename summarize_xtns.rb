6d693ca0b6dcf8708b40c64dac6f65f497998a0e
Ben Prew <ben.prew@gmail.com>
2009-07-12 18:23:41 -0600

    convert summarize_xtbs.rb to use Logger::Application

--- summarize_xtns.rb
+++ summarize_xtns.rb
@@ -7,28 +7,34 @@ require 'dm-core'
 require 'mtg/db'
 require 'logger'
 
-log = Logger.new('/tmp/summarize_xtns.rb', 10, 1024000)
-log.info "Program Started"
-log.info "Deleting xtns"
-repository(:default).adapter.execute("DELETE FROM xtns")
+class XtnSummarizer < Logger::Application
 
-log.info "Inserting new xtns"
-repository(:default).adapter.execute(%Q{
-INSERT INTO xtns
-  SELECT card_no, date(end_time), external_item_id, price, 'AUCTION', cards_in_item
-  FROM external_items
-  WHERE card_no IS NOT NULL AND price IS NOT NULL})
+  def initialize
+    super('XtnSummarizer')
+    set_log('/tmp/summarize_xtns.rb.log', 10, 2048000)
+  end
 
-log.info "Deleting xtns_by_card_day"
-repository(:default).adapter.execute("DELETE FROM xtns_by_card_day")
+  def run
+    @log.info "Deleting xtns"
+    repository(:default).adapter.execute("DELETE FROM xtns")
+    
+    @log.info "Inserting new xtns"
+    repository(:default).adapter.execute(%Q{
+    INSERT INTO xtns
+      SELECT card_no, date(end_time), external_item_id, price, 'AUCTION', cards_in_item
+      FROM external_items
+      WHERE card_no IS NOT NULL AND price IS NOT NULL})
+    
+    @log.info "Deleting xtns_by_card_day"
+    repository(:default).adapter.execute("DELETE FROM xtns_by_card_day")
+    
+    @log.info "Inserting into xtns_by_card_day"
+    repository(:default).adapter.execute(%Q{
+    INSERT INTO xtns_by_card_day
+      SELECT card_no, date, sum(price) as price, sum(xtns) as xtns
+      FROM xtns
+      GROUP BY card_no, date})
+  end
+end
 
-log.info "Inserting into xtns_by_card_day"
-repository(:default).adapter.execute(%Q{
-INSERT INTO xtns_by_card_day
-  SELECT card_no, date, sum(price) as price, sum(xtns) as xtns
-  FROM xtns
-  GROUP BY card_no, date})
-
-
-log.info "Program Ended"
-log.close
+XtnSummarizer.new().start()
