cd8c99336bb9248accc30c5385c3b6c608b7f6c3
Ben Prew <ben.prew@gmail.com>
2009-07-12 18:29:01 -0600

    convert card matching to use sequel instead of datamapper

--- match_xtns.rb
+++ match_xtns.rb
@@ -2,22 +2,20 @@
 
 $:.unshift File.dirname(__FILE__) + '/lib'
 
+require 'rubygems'
+require 'sequel'
+require 'mtg/sql_db'
+
 require 'trie'
-require 'mtg/db'
 require 'logger'
-require 'mtg/external_item'
-require 'mtg/possible_match'
 require 'mtg/trie_matcher'
 
 class XtnMatcher < Logger::Application
+  include SqlDb
 
   def initialize
     super('XtnMatcher')
     set_log('/tmp/match_xtns.rb.log', 10, 2048000)
-  end
-
-  def q(sql, bind_params = [])
-    return repository(:default).adapter.query(sql, bind_params)
   end
 
   def run
@@ -25,18 +23,21 @@ class XtnMatcher < Logger::Application
     match = 0
     possible_match = 0
 
-   @log.level = Logger::INFO
+    @log.level = Logger::INFO
+
     @log.info("Building matcher")
     @matcher = TrieMatcher.new(@log)
 
     @log.info("Loading external items")
-    ext_items = q('SELECT description, external_item_id FROM external_items e LEFT OUTER JOIN possible_matches pm USING (external_item_id) WHERE e.card_no IS null and pm.card_no IS null GROUP BY external_item_id')
+    ext_items = DB[:external_items].select(:description, :external_item_id).filter(:has_match_been_attempted => 0)
+
+    die
     
-    @log.info("Matching #{ext_items.length} items")
+    @log.info("Matching #{ext_items.count} items")
     ext_items.each do |item|
-      possible_matches = @matcher.match(item)
+      possible_matches = @matcher.match(item[:description])
       if !possible_matches || possible_matches.length == 0
-        @log.debug "no matches for #{item['description']}"
+        @log.debug "no matches for #{item[:description]}"
         no_match += 1
       elsif possible_matches.length == 1
         @log.debug "matching card: #{possible_matches[0]}"
@@ -44,21 +45,21 @@ class XtnMatcher < Logger::Application
         match += 1
       else
         @log.debug "saving #{possible_matches.length} possible matches"
-        _save_possible_matches(item['external_item_id'], possible_matches)
+        _save_possible_matches(item[:external_item_id], possible_matches)
         possible_match += 1
       end
+
+      DB[:external_items].filter(:external_item_id => item[:external_item_id]).update(:has_match_been_attempted => 1)
     end
 
-    @log.info("Matched: #{match} : #{sprintf '%d%%', ((match + 0.0) / ext_items.length) * 100}")
+    @log.info("Matched: #{match} : #{sprintf '%d%%', ((match + 0.0) / ext_items.count) * 100}")
     @log.info("No Match: #{no_match}")
     @log.info("Possible Match: #{possible_match}")
   end
 
   def _match_card(item, card_no)
-    e = ExternalItem.get(item['external_item_id'])
-    e.card_no = card_no
-    e.cards_in_item = _cards_in_description(item['description'])
-    e.save
+    e = DB[:external_items].filter(:external_item_id => item[:external_item_id])
+    e.update(:card_no => card_no, :cards_in_item => _cards_in_description(item[:description]))
   end
 
   def _cards_in_description(description)
@@ -72,14 +73,10 @@ class XtnMatcher < Logger::Application
 
   def _save_possible_matches(ext_item_id, possible_matches)
     @log.debug "deleting old matches"
-    repository(:default).adapter.execute('DELETE FROM possible_matches WHERE external_item_id = ?', [ext_item_id])
+    DB[:possible_matches].filter(:external_item_id => ext_item_id).delete
     @log.debug "inserting new matches"
     possible_matches.each do |pm|
-      PossibleMatch.create(
-        :external_item_id => ext_item_id,
-        :card_no => pm,
-        :score => 1
-      ).save
+      DB[:possible_matches].insert(:external_item_id => ext_item_id, :card_no => pm, :score => 1)
     end
   end
 end
