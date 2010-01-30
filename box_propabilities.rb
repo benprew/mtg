e5d6a17fe160c251a9d3d1da9e77deccbc3f222e
Ben Prew <ben.prew@gmail.com>
2010-01-30 11:29:27 -0800

    update the box_propabilities to include foils and better reporting

--- box_propabilities.rb
+++ box_propabilities.rb
@@ -10,31 +10,40 @@ set_name = ARGV[0]
 
 include SqlDb
 
-@cards = DB[:cards].inner_join(:card_prices, :card_no => :card_no).select(:name, :price).filter(:set_name => set_name)
+@cards = DB[:cards].left_outer_join(:card_prices, :card_no => :card_no).select(:name, :price).filter(:set_name => set_name).filter(~:name.like('%Foil%'))
 @rares = @cards.filter(:rarity => 'Rare').all
 @uncommons = @cards.filter(:rarity => 'Uncommon').all
+@foils = DB[:cards].left_outer_join(:card_prices, :card_no => :card_no).select(:name, :price).filter(:set_name => set_name).filter(:name.like('%Foil%')).filter(~:rarity => 'Mythic Rare').all
 
 def rare_total_for_set(set_name)
   total = 0
 
+  total += @foils[(rand() * @foils.length()).to_i][:price] || 0
+  total += @foils[(rand() * @foils.length()).to_i][:price] || 0
+
   (1..36).each do |i|
-    total += @rares[(rand() * @rares.length()).to_i][:price]
-    total += @uncommons[(rand() * @uncommons.length()).to_i][:price]
+    total += @rares[(rand() * @rares.length()).to_i][:price] || 0
+    (1..3).each do |i|
+      total += @uncommons[(rand() * @uncommons.length()).to_i][:price]  || 0
+    end
   end
 
   return total.to_i
 end
+
 
 foo = 0
 prices = []
 
 (1..10000).each do |i|
   box_price = rare_total_for_set(set_name)
-  if box_price >= 100
+  if box_price >= 80
     foo += 1
   end
   prices << box_price
 end
 
 puts "Average box price: $" + (prices.inject(0.0) { |sum, el| sum + el } / prices.size).to_s
-puts foo / 10000.0
+puts "Min box price: $" + prices.min.to_s
+puts "Max box price: $" + prices.max.to_s
+puts "% chance of making >= $80 on a box: " + (foo / 10000.0).to_s
