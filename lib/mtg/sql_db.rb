d56e87d998bbbc6e8efc396efd07a37877677c40
Ben Prew <ben.prew@gmail.com>
2010-02-04 18:21:45 -0800

    smarter caching for the sql_db class, and a spec for external_items

--- lib/mtg/sql_db.rb
+++ lib/mtg/sql_db.rb
@@ -3,14 +3,24 @@ require 'sequel'
 
 module SqlDb
   module_function
-  @@DB =  Sequel.connect('sqlite:///tmp/mtg_test_db')
 
-  configure :production do
-    @@DB = Sequel.connect('mysql://mtg:Tub3rz@localhost/mtg')
-  end
+  @@DB = nil
 
   def db
+    return @@DB unless @@DB.nil?
+
+    configure :test do
+      @@DB =  Sequel.connect('sqlite:///tmp/mtg_test_db')
+    end
+
+    configure :production do
+      @@DB = Sequel.connect('mysql://mtg:Tub3rz@localhost/mtg')
+    end
+
+    configure :development do
+      @@DB = Sequel.connect('mysql://mtg:Tub3rz@localhost/mtg')
+    end
+
     return @@DB
   end
-
 end
