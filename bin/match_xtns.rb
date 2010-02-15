e680dcdd658ed23b6fd7419e5de45b16a138914a
Ben Prew <ben.prew@gmail.com>
2010-02-14 20:57:45 -0800

    move XtnMatcher into own file and write some specs for it

--- bin/match_xtns.rb
+++ bin/match_xtns.rb
@@ -4,7 +4,6 @@ $:.unshift File.dirname(__FILE__) + '/lib'
 
 require 'rubygems'
 require 'optparse'
-require 'logger'
 require 'sinatra/base'
 include Sinatra::Delegator
 
@@ -15,78 +14,7 @@ OptionParser.new do |op|
 end.parse!
 
 # have to require the db after setting the environment
-require 'mtg/sql_db'
-require 'mtg/trie_matcher'
-
-class XtnMatcher < Logger::Application
-  include SqlDb
-
-  def initialize
-    super('XtnMatcher')
-    set_log('/tmp/match_xtns.rb.log', 10, 2048000)
-  end
-
-  def run
-    no_match = 0
-    match = 0
-    possible_match = 0
-
-    @log.level = Logger::INFO
-
-    @log.info("Building matcher")
-    @matcher = TrieMatcher.new(@log)
-
-    @log.info("Loading external items")
-    ext_items = db[:external_items].select(:description, :external_item_id).filter(:has_match_been_attempted => 0)
-
-    count = ext_items.count
-    @log.info("Matching #{count} items")
-    ext_items.each do |item|
-      possible_matches = @matcher.match(item[:description])
-      if !possible_matches || possible_matches.length == 0
-        @log.debug "no matches for #{item[:description]}"
-        no_match += 1
-      elsif possible_matches.length == 1
-        @log.debug "matching card: #{possible_matches[0]}"
-        _match_card(item, possible_matches[0])
-        match += 1
-      else
-        @log.debug "saving #{possible_matches.length} possible matches"
-        _save_possible_matches(item[:external_item_id], possible_matches)
-        possible_match += 1
-      end
-
-      db[:external_items].filter(:external_item_id => item[:external_item_id]).update(:has_match_been_attempted => 1)
-    end
-
-    @log.info("Matched: #{match} : #{sprintf '%d%%', (count.zero? ? 0 : ((match + 0.0) / count)) * 100}")
-    @log.info("No Match: #{no_match}")
-    @log.info("Possible Match: #{possible_match}")
-  end
-
-  def _match_card(item, card_no)
-    e = db[:external_items].filter(:external_item_id => item[:external_item_id])
-    e.update(:card_no => card_no, :cards_in_item => _cards_in_description(item[:description]))
-  end
-
-  def _cards_in_description(description)
-    m = description.match(/(x\s*(\d+)|(\d)+\s*x)/i)
-    if m
-      return m[2] || m[3]
-    else
-      return 1
-    end
-  end
-
-  def _save_possible_matches(ext_item_id, possible_matches)
-    @log.debug "deleting old matches"
-    db[:possible_matches].filter(:external_item_id => ext_item_id).delete
-    @log.debug "inserting new matches"
-    possible_matches.each do |pm|
-      db[:possible_matches].insert(:external_item_id => ext_item_id, :card_no => pm, :score => 1)
-    end
-  end
-end
+require 'mtg/xtn_matcher'
 
 XtnMatcher.new().start()
 
---
+++ spec/xtn_matcher_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/base'
+require 'mtg/xtn_matcher'
+
+describe XtnMatcher do
+  before(:each) do
+    @matcher = XtnMatcher.new
+  end
+
+  it 'determines number of cards in description' do
+    @matcher._cards_in_description('$ MTG $ Magic 2010 x1 Cudgel Troll - FOIL GREAT DEAL').should == 1
+  end
+
+  it "won't ever return cards > 100" do
+    @matcher._cards_in_description('x101').should == 1
+    @matcher._cards_in_description('101x').should == 1
+    @matcher._cards_in_description('2010 x').should == 1
+  end
+end
