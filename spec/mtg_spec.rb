1e197861302328b97ae25708fb16b495d90b20a4
Ben Prew <ben.prew@gmail.com>
2010-11-15 08:17:53 -0800

    new should-dos

--- spec/mtg_spec.rb
+++ spec/mtg_spec.rb
@@ -13,12 +13,14 @@ describe Card do
 
   it "should do" do
     puts %Q{
+      [ ] fix RACK_ENV bug
+      [ ] fix rise from the grave auctions
       [ ] create way to embded datasets possibly by just using div-id?  ie http://mtg.throwingbones.com/datasets/cards-by-price?cardset_id=123  Then the div named cards-by-price would call that dataset
-      [x] price on card detail page should be using card_prices table
       [ ] prices on dashboard should all be using card_prices
       [ ] reporting by ebay userid
       [ ] re-work main page to be same as monthly dashboard
       [ ] should use Sequel for everything
+      [x] price on card detail page should be using card_prices table
     }
   end
 
