a24df7fb5ff3af6a4a3bc7bfee6cfd8f1df77d57
Ben Prew <ben.prew@gmail.com>
2011-01-23 22:33:31 -0800

    remove datamapper card object

--- bin/create_foil_cards.rb
+++ bin/create_foil_cards.rb
@@ -4,7 +4,7 @@ $:.unshift File.dirname(__FILE__) + '/../lib'
 
 require 'rubygems'
 require 'mtg/matcher'
-require 'mtg/card'
+require 'mtg/sql_card'
 require 'mtg/db'
 
 m = Matcher.new()
@@ -16,20 +16,19 @@ File.new(ARGV[0]).readlines.each do |line|
   h = {}
   matches.each do |match|
     next unless match[1] > 2
-    c = Card.get(match[0])
+    c = Card.find(match[0])
     next if c.name == 'Foil'
     next if h.has_key?(c.name)
     h[c.id] = match[1]
   end
   if h.keys.length == 1
-    c = Card.get(h.keys[0])
-    new_card = Card.first_or_create( :name => c.name + " Foil",
-                                     :set_name => c.set_name,
-                                     :casting_cost => c.casting_cost,
-                                     :type => c.type,
-                                     :rarity => c.rarity
-                                     )
-    new_card.save
+    c = Card.find(h.keys[0])
+    Card.find_or_create(
+      :name => c.name + " Foil",
+      :set_name => c.set_name,
+      :casting_cost => c.casting_cost,
+      :type => c.type,
+      :rarity => c.rarity ).save
   else
     warn h.keys.join("\t")
   end
