504eb691ef3769e07b0d57540ee98ff6f7356b74
Ben Prew <ben.prew@gmail.com>
2011-02-26 18:46:19 -0800

    reap previous unicorn instance on deployment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,4 +17,27 @@ pid "tmp/pids/unicorn.pid"
 
 # Set log file paths
 stderr_path "log/unicorn.stderr.log"
-stdout_path "log/unicorn.stdout.log" 
+stdout_path "log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
+  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
