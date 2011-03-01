acf9a279c914355df84cf5e68289af5cc863c133
Ben Prew <ben.prew@gmail.com>
2011-02-28 22:29:05 -0800

    add foils for besieged and scars

--- bin/import_foils.rb
+++ bin/import_foils.rb
@@ -3,36 +3,23 @@
 $:.unshift File.dirname(__FILE__) + '/../lib'
 
 require 'rubygems'
-require 'optparse'
-require 'sinatra/base'
-include Sinatra::Delegator
+require 'mtg'
 
-options = {}
-
-OptionParser.new do |op|
-  op.on("-c cardset", "filename containing the cardset (text spoiler)") { |val| @cardset_file = val }
-  op.on('-e env')    { |val| set :environment, val.to_sym }
-end.parse!
-
-# have to require the db after setting the environment
-require 'mtg/models/card'
-
-File.new(@cardset_file).readlines.each do |line|
-  next if line.match /^#/	
+File.open(ARGV[0]).each do |line|
+  next if line.match /^#/
   (card_name, set_name) = line.split(/\|/)
   set_name.chop!
-  
+
+  cardset = Cardset.first(:name => set_name)
   orig_card = Card.first(:name => card_name)
 
   warn "Creating foil card #{card_name} Foil : #{set_name}"
-  foil = Card.find_or_create(:name => card_name + ' Foil', :set_name => set_name )
+  foil = Card.find_or_create(:name => card_name + ' Foil', :cardset_id => cardset.id)
   foil.update(
     :name => card_name + ' Foil',
     :casting_cost => orig_card.casting_cost,
     :rules_text => orig_card.rules_text,
     :pow_tgh => orig_card.pow_tgh,
-    :type => orig_card[:type],
-    :rarity => orig_card.rarity,
-    :set_name => set_name
-    )
+    :card_type => orig_card.card_type,
+    :rarity => orig_card.rarity )
 end
