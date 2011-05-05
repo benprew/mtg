e53e619192b0f3ce8bdc3262ff94bdd625475677
Ben Prew <ben.prew@gmail.com>
2011-05-05 16:53:43 -0700

    special sorting to match gatherer output

--- bin/import_card_images.pl
+++ bin/import_card_images.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 my $cnt = 0;
+my @cards;
 
 # the process is:
 # 1. Download visual spoiler, sorted by card name
@@ -11,13 +12,25 @@ my $cnt = 0;
 # 3. Run this script with the card names as input
 # 4. Copy the directory (ex. newphyrexia) to public/sets
 
+# For some reason, Gatherer sorts cards as if the ' wasn't there, ex:
+# Phyrexian Oaf
+# Phyrexia's Life
+
 while ( my $line = <> ) {
     $line = lc $line;
-    $line =~ s/[^a-z0-9]//g;
+    $line =~ s/[^a-z0-9 ]//g;
+    push @cards, $line;
+}
+
+@cards = sort { $a cmp $b } @cards;
+
+for my $card (@cards)
+{
+    $card =~ s/\s+//g;
 
     my $file = $cnt == 0 ? "Image.ashx" : "Image($cnt).ashx";
 
-    rename $file, "$line.jpg" or die "$file: $!";
+    rename $file, "$card.jpg" or die "$file: $!";
 
     $cnt++;
 }
